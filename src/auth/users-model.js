c4fb0b72bc13278b149f5d78927411815f8f966e
Corey Marchand <corey.marchand@me.com>
2019-10-30 19:48:15 -0700

    setup done

---
+++ src/auth/users-model.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const middleware = require('./middleware');
+
+const users = new mongoose.Schema({
+  username: {type:String, required:true, unique:true},
+  password: {type:String, required:true},
+  email: {type: String},
+  role: {type: String, default:'user', enum: ['admin','editor','user']},
+});
+
+users.pre('save', async function() {
+  if (this.isModified('password'))
+  {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+});
+
+users.statics.createFromOauth = function(email) {
+
+  if(! email) { return Promise.reject('Validation Error'); }
+
+  return this.findOne( {email} )
+    .then(user => {
+      if( !user ) { throw new Error('User Not Found'); }
+      console.log('Welcome Back', user.username);
+      return user;
+    })
+    .catch( error => {
+      console.log('Creating new user');
+      let username = email;
+      let password = 'none';
+      return this.create({username, password, email});
+    });
+
+};
+
+users.statics.authenticateBasic = function(auth) {
+  let query = {username:auth.username};
+  return this.findOne(query)
+    .then( user => user && user.comparePassword(auth.password) )
+    .catch(error => {throw error;});
+};
+
+users.methods.comparePassword = function(password) {
+  return bcrypt.compare( password, this.password )
+    .then( valid => valid ? this : null);
+};
+
+users.statics.authenticateToken = function(token){
+  let parsedToken = jwt.verify(token, process.env.SECRET);
+  // if(Date.now() >= 15 * 60){
+  //   return _authError();
+  // }
+  return this.findOne({_id: parsedToken.id});
+}
+
+
+users.methods.generateToken = function() {
+
+  let token = {
+    id: this._id,
+    role: this.role,
+  };
+  let options = {}
+  options.expiresIn = '7m';
+
+  return jwt.sign(token, process.env.SECRET, options);
+};
+
+module.exports = mongoose.model('users', users);
